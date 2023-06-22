d30a19c977fd62a11fa8f9f3ec5f172893be65e6
OllinDesigns <cesarvergara@tutanota.com>
2023-06-22 12:25:09 +0200

    for mongo2

--- my-node-project/borrador/validateDivide.notest.js
+++
@@ -1,8 +0,0 @@
-const {divide } = require('../app/matematico')
-
-describe(`testing the function divide`, () => {
-
-  test('divide 6 / 2 to equal 3', () => {
-    expect(divide(6, 2)).toBe(3);
-  });
-});
--- my-node-project/borrador/validateMultiply.notest.js
+++
@@ -1,7 +0,0 @@
-const {multiply} = require('../app/matematico')
-
-test('multiply 4 * 4 to equal 16', () => {
-  expect( multiply(4, 4)).toBe(16);
-});
-
-// console.log("Subtraction:", subtract(20, 5, 3)); // Output: 12
